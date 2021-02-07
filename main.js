f1c253544015cab64f4d832d06f244efa7dafc0b
Akseli <s2100140@edu.bc.fi>
2021-02-07 19:06:37 +0200

    making it mobile friendly

--- main.js
+++ main.js
@@ -5,15 +5,17 @@ console.log("Hi! Watcha doing here?");
 const body = document.querySelector("body");
 
 let header = document.querySelector(".container");
-
+let otsikko1 = document.querySelector("h1");
 let fixed = header.offsetTop;
 
 const stickIt = (event) => {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     header.classList.add("fixed");
+    otsikko1.classList.add("topPadding");
     // header.getElementsByClassName.background = "#ffffffb3";
   } else {
     header.classList.remove("fixed");
+    otsikko1.classList.remove("topPadding");
   }
 };
 
